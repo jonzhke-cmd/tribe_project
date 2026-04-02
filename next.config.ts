440823e56fd1aaf49865a50630ebbaa7937be102
jonzhke <jonzhke@github.com>
2026-04-02 00:55:16 +0000

    fix: allow Next.js Image to render external unsplash domains

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
